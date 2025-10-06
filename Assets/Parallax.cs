766672f0196d1d737f414d79b7b27d06e02f21b8
Iago Rosa <iagoimportantthings@gmail.com>
2025-10-06 16:57:26 -0300

    chore: initial Unity project

---
+++ Assets/Parallax.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    private float lenght;
+    private float movingSpeed = 5f;
+    public float parallaxEffect;
+    void Start()
+    {
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime * parallaxEffect;
+        if (transform.position.x < -lenght)
+        {
+            transform.position = new Vector3(lenght, transform.position.y, transform.position.z);
+        }
+
+    }
+}
