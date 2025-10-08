3c4dda73ad78f616332ac7e617aaa9f77eef3b37
Iago Rosa <iagoimportantthings@gmail.com>
2025-10-08 20:33:44 -0300

    feat: gerando inimigos

---
+++ Assets/Scripts/Inimigo.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    private float movingSpeed = 2.5f;
+    private float verticalBound = 3.5f;
+    private float horizontalBound = 4.5f;
+    private Vector2 direction;
+
+    private void Start()
+    {
+        EscolherNovaDirecao();
+    }
+
+    private void Update()
+    {
+        transform.Translate(direction * movingSpeed * Time.deltaTime);
+
+        var position = transform.position;
+
+        if(position.x > horizontalBound || position.x < -horizontalBound)
+        {
+            movingSpeed += 0.2f;
+            direction.x *= -1;
+            position.x = Mathf.Clamp(position.x, -horizontalBound, horizontalBound);
+        }
+        if(position.y > verticalBound || position.y < -verticalBound)
+        {
+            movingSpeed += 0.2f;
+            direction.y *= -1;
+            position.y = Mathf.Clamp(position.y, -verticalBound, verticalBound);
+        }
+
+        transform.position = position;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    void EscolherNovaDirecao()
+    {
+        direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject inimigoPrefab;
+
+    public Transform point1;
+    public Transform point2;
+    public Transform point3;
+
+    private float timer = 0.0f;
+    private float waitTime = 1.0f;
+
+    private void Start()
+    {
+        gerarInimigo();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= waitTime)
+        {
+            timer = 0.0f;
+            int randomPoint = Random.Range(1, 4);
+            if (randomPoint != 1)
+            {
+                gerarInimigo();
+                waitTime -= 0.001f;
+            }
+        }
+
+    }
+
+    void gerarInimigo()
+    {
+        int randomPoint = Random.Range(1, 4);
+        switch (randomPoint)
+        {
+            case 1:
+                Instantiate(inimigoPrefab, point1.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(inimigoPrefab, point2.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(inimigoPrefab, point3.position, Quaternion.identity);
+                break;
+        }
+    }
+}
