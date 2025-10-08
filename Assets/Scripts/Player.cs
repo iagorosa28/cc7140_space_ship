20175e3ea7b5498d7cacd58d93cb6e688fbf278c
Iago Rosa <iagoimportantthings@gmail.com>
2025-10-08 18:53:41 -0300

    feat: melhorando movimento do player e adicionando tiros

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,14 @@ public class Player : MonoBehaviour
 {
     public KeyCode moveUp = KeyCode.UpArrow;
     public KeyCode moveDown = KeyCode.DownArrow;
+    public KeyCode moveRight = KeyCode.RightArrow;
+    public KeyCode moveLeft = KeyCode.LeftArrow;
     public float movingSpeed = 5.0f;
     public float verticalBound = 3.5f;
+    public float horizontalBound = 4.5f;
+
+    public GameObject tiroPrefab;
+    public Transform firePoint;
 
     private Rigidbody2D playerRb2d;
     void Start()
@@ -15,22 +21,44 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float direction = 0.0f;
+        float verticalDirection = 0.0f;
+        float horizontalDirection = 0.0f;
+
         if (Input.GetKey(moveUp))
         {
-            direction = 1.0f;
+            verticalDirection = 1.0f;
         }
         else if (Input.GetKey(moveDown))
         {
-            direction = -1.0f;
+            verticalDirection = -1.0f;
+        }else if (Input.GetKey(moveRight))
+        {
+            horizontalDirection = 1.0f;
+        }else if (Input.GetKey(moveLeft))
+        {
+            horizontalDirection = -1.0f;
         }
 
-        var velocity = playerRb2d.linearVelocity;
-        velocity.y = direction * movingSpeed;
-        playerRb2d.linearVelocity = velocity;
+        var vVelocity = playerRb2d.linearVelocity;
+        vVelocity.y = verticalDirection * movingSpeed;
+        playerRb2d.linearVelocity = vVelocity;
 
-        var position = transform.position;
-        position.y = Mathf.Clamp(position.y, -verticalBound, verticalBound);
-        transform.position = position;
+        var hVelocity = playerRb2d.linearVelocity;
+        hVelocity.x = horizontalDirection * movingSpeed;
+        playerRb2d.linearVelocity = hVelocity;
+
+        var vPosition = transform.position;
+        vPosition.y = Mathf.Clamp(vPosition.y, -verticalBound, verticalBound);
+        transform.position = vPosition;
+
+        var hPosition = transform.position;
+        hPosition.x = Mathf.Clamp(hPosition.x, -horizontalBound, horizontalBound);
+        transform.position = hPosition;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            firePoint = transform.Find("FirePoint");
+            Instantiate(tiroPrefab, firePoint.position, Quaternion.identity);
+        }
     }
 }
---
+++ Assets/Scripts/Tiro.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Tiro : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float maxDistance = 9.0f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        if (Mathf.Abs(transform.position.x) > maxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Inimigo"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
